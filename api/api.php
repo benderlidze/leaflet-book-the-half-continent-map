76e288ca97569d825a2c0e7bec8fc1b23d8ef96d
Serg <benderlio@gmail.com>
2024-06-29 09:13:00 +0200

    up

--- api/api.php
+++ api/api.php
@@ -1,5 +1,9 @@
 <?php
 header("Content-Type: application/json");
+//disable cors
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
 
 try {
     // Database connection
